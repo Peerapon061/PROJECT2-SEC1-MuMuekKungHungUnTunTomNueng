73d0944a10c6be02b2d781031b25227ef135a392
seumpicco1 <88130332+seumpicco1@users.noreply.github.com>
2023-03-18 15:17:52 +0700

    Add files via upload

---
+++ new/src/components/Tell.vue
@@ -0,0 +1,15 @@
+<script setup>
+                const props = defineProps({
+                 show: { type:Boolean,  default: false} } ) 
+</script>
+ 
+<template>
+<div  class ="tell-container" :class="show ? 'show' : ''">
+    <p>Ready ??????????</p>
+
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
---
+++ new/src/components/Word.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+     letters: { type:Array,  required: true } , 
+     correctLetters: {type:Array ,default :[] }} ) 
+</script>
+ 
+<template>
+<div class ="word">
+    <span class="letter" v-for="(word,index) in letters" :key="index" >
+    
+    {{ correctLetters.includes(word) ? word :'' }}
+    
+    </span>
+  
+
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
