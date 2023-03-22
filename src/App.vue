2a7ddacb1c51a7bfddd9b587cc487acb7df8522e
Peerapon061 <peerapon.sr@mail.kmutt.ac.th>
2023-03-22 21:55:35 +0700

    get data for game

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
 import startGame from'./pages/startGame.vue';
 import SelectCategories from './pages/SelectCategories.vue';
 import game from './pages/hangman.vue'
+import play from './pages/playhangman.vue'
 </script>
 
 <template>
 <div class="w-screen h-screen "> 
     <!-- <startGame/>   -->
     <!-- <SelectCategories/>   -->
-  <div class=""><game/></div>  
-  
+  <!-- <div class=""><game/></div>   -->
+  <play/>
 </div>
 </template>
 
--- src/pages/playhangman.vue
+++ src/pages/playhangman.vue
@@ -1,10 +1,30 @@
 <script setup>
+import {onMounted,ref} from 'vue';
+import { getCategories } from '../composable/getCategories';
 
+const cataForGame=ref([])
+const SelectedCata=ref([])
+
+
+console.log(SelectedCata)
+
+onMounted(async()=>{
+    cataForGame.value=await getCategories()
+    console.log(cataForGame)
+})
+const showWord=()=>{
+    console.log(SelectedCata.value);
+}
 </script>
  
 <template>
 <div>
-
+    <select v-model="SelectedCata" name="categories" id="categories">
+        <option v-for="(cata,index) in cataForGame" :key='cata.id' class="text-white" :value="cata.vocabs">{{ cata.CategoryName }}</option>
+    </select>
+    <button @click="showWord">show selected word</button>
+    <!-- <v-select></v-select> -->
+    <!-- <button @click="SelectCata(cata)">submit</button> -->
 </div>
 </template>
  
